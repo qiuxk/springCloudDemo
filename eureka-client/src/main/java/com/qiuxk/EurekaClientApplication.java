3e7654537ccb3a7942568dd49aef660639d010f3
qiuxk <qxk1101@163.com>
2019-04-23 13:26:06 +0800

    添加eureka client到eureka server注册的demo

--- eureka-client/src/main/java/com/qiuxk/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/qiuxk/EurekaClientApplication.java
@@ -1,16 +1,31 @@
 package com.qiuxk;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 /**表明 生产者 的身份**/
 @EnableEurekaClient
+@RestController
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+
+    @Value("${server.port}")
+    private  String port;
+
+
+    @RequestMapping("/hello")
+    public String home(@RequestParam String name){
+        return "I am "+name+" my post is "+port;
     }
 
 }
