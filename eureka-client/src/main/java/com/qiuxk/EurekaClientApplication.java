0f0d59531620d2498e70aba9d2886b162dd6252c
qiuxk <qxk1101@163.com>
2019-04-23 10:22:18 +0800

    Initial commit

---
+++ eureka-client/src/main/java/com/qiuxk/EurekaClientApplication.java
@@ -0,0 +1,16 @@
+package com.qiuxk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+/**表明 生产者 的身份**/
+@EnableEurekaClient
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+}
