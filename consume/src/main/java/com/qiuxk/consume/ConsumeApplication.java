a05f32f59845fe7fdcd518d2bee00ad640c2c5c5
qiuxk <qxk1101@163.com>
2019-05-07 17:25:04 +0800

    消费者模块添加Hystrix 熔断器等相关配置

--- consume/src/main/java/com/qiuxk/consume/ConsumeApplication.java
+++ consume/src/main/java/com/qiuxk/consume/ConsumeApplication.java
@@ -4,12 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-//@EnableEurekaClient只适用于Eureka作为注册中心，@EnableDiscoveryClient 可以是其他注册中心。
+/**@EnableEurekaClient 只只适用于Eureka作为注册中心，@EnableDiscoveryClient 可以是其他注册中心。**/
 @EnableDiscoveryClient
+/**开启熔断器**/
+@EnableHystrix
+/**开启hystri仪表盘 显示Hystri各项指标信息**/
+@EnableHystrixDashboard
 public class ConsumeApplication {
 
     public static void main(String[] args) {
--- consume/src/main/java/com/qiuxk/consume/controller/HelloController.java
+++ consume/src/main/java/com/qiuxk/consume/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.qiuxk.consume.controller;
 
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.qiuxk.consume.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +14,18 @@ public class HelloController {
     @Autowired
     private HelloService helloService;
 
+
+
     @RequestMapping(value = "/hi")
+    /**断路器配置，当hi服务无法调用时 则调用hiError方法**/
+    @HystrixCommand(fallbackMethod = "hiError")
     public  String hi(@RequestParam String name){
         return helloService.hiService(name);
+    }
+
+
+    @RequestMapping(value = "hiError")
+    public  String hiError(String name){
+        return "hiError "+ name;
     }
 }
