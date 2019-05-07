cf21e785d7d399813c52e1c0fa1d541e78c49d67
qiuxk <qxk1101@163.com>
2019-05-07 16:55:14 +0800

    添加readme文件

--- consume/src/main/java/com/qiuxk/consume/service/HelloService.java
+++ consume/src/main/java/com/qiuxk/consume/service/HelloService.java
@@ -1,4 +1,21 @@
 package com.qiuxk.consume.service;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class HelloService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    public  String hiService(String name){
+
+        /**消费SERVICE-FIRST 中的hello服务**/
+
+     return     restTemplate.getForObject("http://SERVICE-FIRST/hello?name=" + name,String.class);
+    }
 }
